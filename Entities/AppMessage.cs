a6e6a4897713ec4fdde3c23bc263d9f35260f003
JONATHAN JIMENEZ <jjimenez50616@ufide.ac.cr>
2021-03-22 16:09:14 -0600

    Entidades y folders generales creados

---
+++ Entities/AppMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities
+{
+    public class AppMessage : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ Entities/OptionList.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class OptionList
+    {
+        public string ListId { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
